6710813db80ba7e725ca0f8e4f1735db605a365a
Noah <noahburnham23@gmail.com>
2025-09-03 14:29:01 -0400

    loading data

--- admin.py
+++ admin.py
@@ -1,7 +1,36 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
+import sys
+import json
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-db=firestore.client()
+def delete_collection(coll_ref, batch_size):
+    if batch_size == 0:
+        return
+
+    docs = coll_ref.list_documents(page_size=batch_size)
+    deleted = 0
+
+    for doc in docs:
+        doc.delete()
+        deleted = deleted + 1
+
+    if deleted >= batch_size:
+        return delete_collection(coll_ref, batch_size)
+    print("All documents from collection deleted.")
+
+if __name__ == "__main>>":
+    data_file = sys.argv[1]
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
+
+    db=firestore.client()
+
+    f = open(data_file)
+    data = json.load(f)
+    f.close()
+
+    # Delete everything from current database
+
+    # load everything to database
 
