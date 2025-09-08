2c491e5609a3289a2d8ccac730d3eb9b837f2073
Noah <noahburnham23@gmail.com>
2025-09-08 18:24:31 -0400

    compound queries enabled in query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -20,66 +20,54 @@ class QueryPlan:
 def run_fn(db, plan: QueryPlan):
     #TODO: This will capture the operator of a well formed single query. We can use a
     # switch after this to tailor query functionality for each operator
-    if len(plan.filters) == 1:
-        # if OF or of is entered this will capture it and normalize it to OF
-        single_filter_operand = plan.filters[0][1].op.upper()
-        '''
-        # Special handling for OF operator
-        if single_filter_operand == "OF":
-            town_name = plan.filters[0][1].value  # e.g., "Burlington"
-            field_to_get = plan.filters[0][1].field  # e.g., "altitude"
-            
-            # Find the town by name (case-insensitive)
-            # Get all towns and filter by case-insensitive name match
-            all_towns = db.collection("Vermont_Municipalities").stream()
-            docs = []
-            for doc in all_towns:
-                town_data = doc.to_dict()
-                if town_data.get("Town_Name", "").lower() == town_name.lower():
-                    docs.append(doc)
-                    break  # Found the town, stop searching
-            
-            if docs:
-                town_data = docs[0].to_dict()
-                firestore_field = normalize_field(field_to_get)
-                field_value = town_data.get(firestore_field)
-                return [field_value] if field_value is not None else []
-            else:
-                return []  # Town not found
-    '''
+
     # Start with collection reference
     query = db.collection("Vermont_Municipalities")
 
     # Apply all filters from QueryPlan
-    for _, f in plan.filters:
-        if f.op == "OF":
-            query = query.where(filter=FieldFilter("town_name", "==", f.value))
-            # Apply ordering if specified
-            if plan.order_by:
-                query = query.order_by(plan.order_by)
+    for connector, f in plan.filters:
+        if connector == "":
+            # this is the first query and will always run
+            if f.op == "OF":
+                query = query.where(filter=FieldFilter("town_name", "==", f.value))
+                # Apply ordering if specified
+                if plan.order_by:
+                    query = query.order_by(plan.order_by)
 
-            # Apply limit if specified
-            if plan.limit:
-                query = query.limit(plan.limit)
+                # Apply limit if specified
+                if plan.limit:
+                    query = query.limit(plan.limit)
 
-            # Execute and return dicts instead of snapshots
-            docs = list(query.stream())
-            for doc in docs:
-                doc.to_dict()
-            return docs[0].get(f.field)
-        else:  # operator is ==, <, >, !=
+                # Execute and return dicts instead of snapshots
+                docs = list(query.stream())
+                if docs:
+                    docs[0].to_dict()
+                    return docs[0].get(f.field)
+                else:
+                    return []
+            else:  # operator is ==, <, >, !=
+                query = query.where(filter=FieldFilter(f.field, f.op, f.value))
+                # Apply ordering if specified
+                if plan.order_by:
+                    query = query.order_by(plan.order_by)
+
+                # Apply limit if specified
+                if plan.limit:
+                    query = query.limit(plan.limit)
+
+                # Execute and return dicts instead of snapshots
+                docs = list(query.stream())
+                # return [doc.to_dict() | {"id": doc.id} for doc in docs]
+        elif connector == "AND":
+            # add another "where" to the query
+            # can assume here that the operator is not "OF"
             query = query.where(filter=FieldFilter(f.field, f.op, f.value))
-            # Apply ordering if specified
-            if plan.order_by:
-                query = query.order_by(plan.order_by)
-
-            # Apply limit if specified
-            if plan.limit:
-                query = query.limit(plan.limit)
-
-            # Execute and return dicts instead of snapshots
-            docs = list(query.stream())
-            return [doc.to_dict() | {"id": doc.id} for doc in docs]
+        else: # connector == "OR":
+            # append the new docs to the existing docs
+            # can assume here that the operator is not "OF"
+            new_query = (db.collection("Vermont_Municipalities")
+                         .where(filter=FieldFilter(f.field, f.op, f.value)))
+            docs.append(list(new_query.stream()))
 
 
     # Apply ordering if specified
